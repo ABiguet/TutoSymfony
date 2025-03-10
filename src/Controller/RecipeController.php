5af0622ed602962339273b8698634c6a9cf963d1
Aurelien Biguet <10219840+ABiguet@users.noreply.github.com>
2025-03-10 17:18:43 +0100

    Update recipe

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,23 +12,25 @@ use Symfony\Component\Routing\Attribute\Route;
 final class RecipeController extends AbstractController
 {
     #[Route('/recette', name: 'recipe.index')]
-    public function index(Request $request): Response
+    public function index(Request $request, RecipeRepository $recipeRepository): Response
     {
+        $recipeList = $recipeRepository->findAll();
         return $this->render('recipe/index.html.twig', [
             'controller_name' => 'RecipeController',
+            'recipes' => $recipeList,
         ]);
     }
 
     #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
-    public function show(Request $request, string $slug, int $id): Response
+    public function show(Request $request, RecipeRepository $recipeRepository, string $slug, int $id): Response
     {
+        $recipe = $recipeRepository->find($id);
+        if (!$recipe) {
+            throw $this->createNotFoundException('La recette demandée n\'existe pas');
+        }
         return $this->render('recipe/show.html.twig', [
-            'slug' => $slug,
-            'id' => $id,
-            'person' => [
-                'name' => 'John Doe',
-                'age' => 30,
-            ],
+            'controller_name' => 'RecipeController',
+            'recipe' => $recipe,
         ]);
     }
 }
