ea9027168bddc0ff95b4af2189fcfda069a84b76
Aurelien Biguet <aurelen.biguet@icloud.com>
2025-06-23 12:29:50 +0200

    Refactor HomeController to finalize class and specify HTTP method for route

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,13 +7,13 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController {
-
-    #[Route('/', name: 'home')]
-    function index (): Response {
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home', methods: ['GET'])]
+    public function index(): Response
+    {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
-
 }
